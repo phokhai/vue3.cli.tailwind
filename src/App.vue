ef29b7f8c957585e569642ec9881a535ef704ba9
T480s <techcom.ec20@gmail.com>
2020-12-31 18:07:07 +0700

    App ButtonBar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <TopBar></TopBar>
+    <ButtonBar></ButtonBar>
     <!-- <router-view/> -->
   </div>
 </template>
 
 <script lang="ts">
-
-import { defineComponent } from "vue";
-import TopBar from "@/views/_layout/TopBar.vue";
+import { defineComponent } from 'vue';
+import TopBar from '@/views/_layout/TopBar.vue';
+import ButtonBar from '@/views/_layout/ButtonBar.vue';
 
 export default defineComponent({
   components: {
     TopBar,
+    ButtonBar,
   },
   setup() {
     return {};
---
+++ src/views/_layout/ButtonBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex place-items-center justify-end pr-6 content-center w-full bg-gray-100 h-10 absolute bottom-0">
+    <span class="hover:scale-110 transform motion-reduce:transform-none">made with by (◍•ᴗ•◍)❤</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
