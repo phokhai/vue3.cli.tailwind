adcf33a4f8b777eb91dc038f211e15ecacac6143
T480s <techcom.ec20@gmail.com>
2020-12-31 17:19:17 +0700

    add .prettierrc

---
+++ src/views/_layout/TopBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-end place-items-center mx-auto shadow-sm bg-blue-500 h-12 max-h-12">
+    <div class="mr-6 cursor-pointer transform hover:scale-110 motion-reduce:transform-none">Logout</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
